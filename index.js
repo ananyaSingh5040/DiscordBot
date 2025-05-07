ef8342a2d089e2198ea4f1d900e03d0abe0e9db9
Ananya Singh <singhananya5040@gmail.com>
2025-05-07 13:19:46 +0530

    07-05-25

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ const client = new Client({
 });
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
+  if(message.content.startsWith('create'))
+  {
+    const url = message.content.split('create ')[1];
+    message.reply({
+        content: "Generating a short ID for " + url
+    })
+  }
+  else{
   message.reply({
     content: "Hello munchkin!",
   });
+}
 });
 
 client.on("interactionCreate", (interaction) => {
