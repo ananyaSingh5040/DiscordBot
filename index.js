a39340842a257f7dc4837c62be6c54e27950f1cc
Ananya Singh <singhananya5040@gmail.com>
2025-05-07 16:00:03 +0530

    07-05-25

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 import axios from "axios";
-import { Client, Events, GatewayIntentBits, messageLink } from "discord.js";
+
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  messageLink,
+  EmbedBuilder,
+} from "discord.js";
 
 const client = new Client({
   intents: [
@@ -14,13 +21,45 @@ client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   if (message.content.startsWith("create")) {
     const url = message.content.split("create ")[1];
-    if (!url) message.reply({ content: "Provide a url :')" });
-    await message.reply({
-      content: "Generating a short ID for " + url,
-    });
+    if (!url) {
+      message.reply({ content: "Provide a url :')" });
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:8001/url",
+        { url: url },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.DISCORD_BOT_JWT}`,
+            "User-Agent": "Discordbot",
+          },
+        }
+      );
+      const shortUrl = response.data.shortUrl;
+      const embed = new EmbedBuilder()
+        .setTitle("Short URL Created")
+        .setDescription("Here's your new link!")
+        .addFields({ name: "Link", value: shortUrl })
+        .setColor("Random");
+      return message.reply({
+        content: "URL shortened successfully!",
+        embeds: [embed],
+      });
+    } catch (err) {
+      console.error(err.message);
+      return message.reply({
+        content: "Something went wrong while generating your short URL :') ",
+      });
+    }
+  } else if (message.content.startsWith("spill")) {
+    message.reply({ content: "**Here's what I can do for you:**\n\n" +
+             "🔗 **Generate URL's** – Get a short URL for any link just type  **'create <your-url>'** .\n" +
+             "🎁 **Ping-Pong** – I can pong you with simple  **'/ping'** .\n" +
+             "*More features coming soon, bestie!*✨" });
   } else {
     message.reply({
-      content: "Hello munchkin!",
+      content: "Hello munchkin!\n *Type 'spill' to know what things I can do!*",
     });
   }
 });
