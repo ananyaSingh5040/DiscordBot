879ed48e39511112b959b040f36688414d7277fc
Ananya Singh <singhananya5040@gmail.com>
2025-05-07 15:12:26 +0530

    07-05-25

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+import axios from "axios";
 import { Client, Events, GatewayIntentBits, messageLink } from "discord.js";
 
 const client = new Client({
@@ -9,20 +10,19 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  if(message.content.startsWith('create'))
-  {
-    const url = message.content.split('create ')[1];
+  if (message.content.startsWith("create")) {
+    const url = message.content.split("create ")[1];
+    if (!url) message.reply({ content: "Provide a url :')" });
+    await message.reply({
+      content: "Generating a short ID for " + url,
+    });
+  } else {
     message.reply({
-        content: "Generating a short ID for " + url
-    })
+      content: "Hello munchkin!",
+    });
   }
-  else{
-  message.reply({
-    content: "Hello munchkin!",
-  });
-}
 });
 
 client.on("interactionCreate", (interaction) => {
